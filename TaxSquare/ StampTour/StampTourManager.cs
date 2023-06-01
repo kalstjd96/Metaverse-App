9919525a5fde978d98ddf1abfcdd1c64e61288c1
Minsung Kim <47016363+kalstjd96@users.noreply.github.com>
2023-06-01 22:34:36 +0900

    Update StampTourManager.cs

--- TaxSquare/ StampTour/StampTourManager.cs
+++ TaxSquare/ StampTour/StampTourManager.cs
@@ -16,3 +16,13 @@ public class StampTourManager : MonoBehaviour
         if (collectedStamps == totalStamps)
         {
             // 모든 스탬프를 모았을 때 포인트 지급 등의 로직 실행
+            ResetStamps();
+        }
+    }
+
+    private void ResetStamps()
+    {
+        // 매일 자정 12시에 호출되어 스탬프 초기화
+        collectedStamps = 0;
+    }
+}
